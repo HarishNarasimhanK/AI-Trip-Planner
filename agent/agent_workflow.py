ff8d29dd5d6bc4d7b39c2fdd2cd74a37021a50bb
Harish Narasimhan K <harishnarasimhan0135@gmail.com>
2025-06-29 19:42:21 +0530

    added tools

--- agent/agent_workflow.py
+++ agent/agent_workflow.py
@@ -2,25 +2,40 @@ from utils.model_loader import ModelLoader
 from prompt_library import SYSTEM_PROMPT
 from langgraph.graph import StateGraph, MessageState, END, START
 from langgraph.prebuilt import ToolNode, tools_condition
+from tools.weather_info_tool import WeatherInfoTool
+from tools.currency_conversion_tools import CurrencyConverterTool
+from tools.place_search_tool import PlaceSearchTool
+from tools.calculator_tool import CalculatorTool
 
 
 class GraphBuilder:
-    def __init__(self, graph):
+    def __init__(self, model_provider : str = "groq"):
+        self.model_loader = ModelLoader(model_provider = model_provider)
+        self.llm = self.model_loader.load_llm()
         self.tools = [
 
         ] 
+        self.weather_tools = WeatherInfoTool()
+        self.place_search_tools = PlaceSearchTool()
+        self.currency_converter_tools = CurrencyConverterTool()
+        self.calculator_tools = CalculatorTool()
 
-
+        self.tools.extend([* self.weather_tools.weather_tools_list,
+                           * self.place_search_tools.weather_tools_list,
+                           * self.currency_converter_tools.weather_tools_list,
+                           * self.calculator_tools.weather_tools_list])
+        self.llm_with_tools = self.llm.bind_tools(tools = self.tools)
         self.system_prompt = SYSTEM_PROMPT
 
-    def agent_function(self):
+    def agent_function(self, state : MessageState):
         """
         Main agent function
         """
         user_question = state["messages"]
         input_question = [self.system_prompt] + user_question
-        response = llm_with_tools.invoke(input_question)
-        return {"Messages" : {response}}
+        response = self.llm_with_tools.invoke(input_question)
+        return {"messages" : [response]}
+
 
     def build_graph(self):
         graph_builder = StateGraph(MessageState)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,4 @@
-from utils.model_loader import ModelLoader
-from prompt_library import SYSTEM_PROMPT
-from langgraph.graph import StateGraph, MessageState, END, START
-from langgraph.prebuilt import ToolNode, tools_condition
+from prompt_library.prompt import SYSTEM_PROMPT
 import streamlit as st
 import datetime
 import requests
@@ -10,20 +7,20 @@ import sys
 BASE_URL = "http://localhost:8000" # Backend Endpoint
 
 st.set_page_config(
-    page_title = "✈️ Your AI Travel Planner",   
+    page_title = "Your AI Travel Planner",   
     layout = "centered",
     initial_sidebar_state = "expanded",
-    page_icon = "✈️"
+    page_icon = "data/images/anywhere_voyager.png"
 )
 st.title("Anywhere Voyager")
 # Inititalizing the chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
-st.header("Hey! I am your friendly AI trip and expense planner.. \nWith My stand Anywhere voyager, I can help you seamlessly😊")
+st.header("Hey! I am your friendly AI trip and expense planner.. \nWith My stand 'Anywhere voyager', I can help you seamlessly😊")
 
-with st.form():
+with st.form(key = "query_form", clear_on_submit = True):
     user_input = st.text_input("Enter your request here..", placeholder = "E.g: Plan a trip to Orlando, Florida")
-    submit_button = st.form_submit_button("➡️")
+    submit_button = st.form_submit_button("✎ Generate")
 
 if submit_button and user_input.strip():
     try:
