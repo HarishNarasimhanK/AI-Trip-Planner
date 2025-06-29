a75f1cb67ec69b298ba69805db0ccdff8ea98724
Harish Narasimhan K <harishnarasimhan0135@gmail.com>
2025-06-29 18:02:40 +0530

    Edited main.py

--- agent/agent_workflow.py
+++ agent/agent_workflow.py
@@ -6,14 +6,31 @@ from langgraph.prebuilt import ToolNode, tools_condition
 
 class GraphBuilder:
     def __init__(self, graph):
-        pass 
+        self.tools = [
+
+        ] 
+
+
+        self.system_prompt = SYSTEM_PROMPT
 
     def agent_function(self):
-        pass
+        """
+        Main agent function
+        """
+        user_question = state["messages"]
+        input_question = [self.system_prompt] + user_question
+        response = llm_with_tools.invoke(input_question)
+        return {"Messages" : {response}}
 
     def build_graph(self):
-        pass 
+        graph_builder = StateGraph(MessageState)
+        graph_builder.add_node("agent", self.agent_function)
+        graph_builder.add_node("tools", ToolNode(tools = self.tools))
+        graph_builder.add_edge(START, "agent")
+        graph_builder.add_conditional_edges("agent", tools_condition)
+        graph_builder.add_edge("tools", "agent")
+        graph_builder.add_edge("agent", END)
 
     def __call__(self):
-        pass
+        return self.build_graph()
         
